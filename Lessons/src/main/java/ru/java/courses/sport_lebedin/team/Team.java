a75826572194a0510e7854df57e3e6a7fe11a560
leb17 <38190385+leb17@users.noreply.github.com>
2019-04-08 10:25:09 +0300

    параметрический полиморфизм для Team/FootballTeam

--- Lessons/src/main/java/ru/java/courses/sport_lebedin/team/Team.java
+++ Lessons/src/main/java/ru/java/courses/sport_lebedin/team/Team.java
@@ -1,12 +1,16 @@
 package ru.java.courses.sport_lebedin.team;
 
 import ru.java.courses.sport_lebedin.team.football.Coach;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public abstract class Team<T extends Athlete> {
+public abstract class Team<T extends Athlete & ScoringPlayer> {
 
     private String name;
 
     private Coach coach;
+
+    private ArrayList<T> players = new ArrayList<>();
 
     public Team(String name) {
         setName(name);
@@ -14,8 +18,51 @@ public abstract class Team<T extends Athlete> {
 
     public abstract int getMaxPlayersCount();
 
+    public int getPlayersCount() {
+        return players.size();
+    }
+
+    public void addPlayer(T player) {
+        if (player == null) {
+            throw new IllegalArgumentException("Игрок не выбран");
+        } else if (players.size() >= getMaxPlayersCount()) {
+            throw new IllegalArgumentException("Команда уже укомплектована");
+        } else
+            players.add(player);
+    }
+
+    public void addPlayers(T...newplayers) {
+        for (T f: newplayers) {
+            if (f == null) {
+                throw new IllegalArgumentException("Один из игроков не выбран");
+            }
+        }
+        if (players.size() + newplayers.length > getMaxPlayersCount()) {
+            throw new IllegalArgumentException("Команда уже укомплектована");
+        } else
+            /*for (FootballPlayer player : newplayers) {
+                this.players.add(player);
+            }*/
+            Collections.addAll(this.players, newplayers);
+    }
+
+    public void removePlayer(T player) {
+        if (player == null) {
+            throw new IllegalArgumentException("Игрок не выбран");
+        } else
+            players.remove(player);
+    }
+
     public String getName() {
         return name;
+    }
+
+    public int getScore() {
+        int sum = 0;
+        for (T player : players) {
+            sum += player.getScore();
+        }
+        return sum;
     }
 
     public final void setName(String name) {
@@ -31,5 +78,43 @@ public abstract class Team<T extends Athlete> {
 
     public void setCoach(Coach coach) {
         this.coach = coach;
+    }
+
+    public ArrayList<T> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<T> players) {
+        this.players = players;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "name='" + name + '\'' +
+                ", coach=" + coach +
+                ", players=" + players +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Team<?> team = (Team<?>) o;
+
+        if (name != null ? !name.equals(team.name) : team.name != null) return false;
+        if (coach != null ? !coach.equals(team.coach) : team.coach != null) return false;
+        return players != null ? players.equals(team.players) : team.players == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (coach != null ? coach.hashCode() : 0);
+        result = 31 * result + (players != null ? players.hashCode() : 0);
+        return result;
     }
 }
--- Lessons/src/main/java/ru/java/courses/sport_lebedin/team/football/FootballTeam.java
+++ Lessons/src/main/java/ru/java/courses/sport_lebedin/team/football/FootballTeam.java
@@ -1,95 +1,22 @@
 package ru.java.courses.sport_lebedin.team.football;
 
 import ru.java.courses.sport_lebedin.team.Team;
-import java.util.ArrayList;
-import java.util.Collections;
 
-public class FootballTeam extends Team {
-
-    private ArrayList<FootballPlayer> players = new ArrayList<>();
+public class FootballTeam extends Team<FootballPlayer> {
 
     private static final int MAX_PLAYERS_COUNT = 20;
-
 
     public FootballTeam(String name) {
         super(name);
     }
 
-    public ArrayList<FootballPlayer> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(ArrayList<FootballPlayer> players) {
-        this.players = players;
-    }
-
-    public int getPlayersCount() {
-        return players.size();
-    }
-
-    public static int getMaxPlayersCount() {
+    public int getMaxPlayersCount() {
         return MAX_PLAYERS_COUNT;
-    }
-
-    public void addPlayer(FootballPlayer player) {
-        if (player == null) {
-            throw new IllegalArgumentException("Игрок не выбран");
-        } else if (players.size() >= getMaxPlayersCount()) {
-            throw new IllegalArgumentException("Команда уже укомплектована");
-        } else
-            players.add(player);
-    }
-
-    public void addPlayers(FootballPlayer...newplayers) {
-        for (FootballPlayer f: newplayers) {
-            if (f == null) {
-                throw new IllegalArgumentException("Один из игроков не выбран");
-            }
-        }
-        if (players.size() + newplayers.length > getMaxPlayersCount()) {
-            throw new IllegalArgumentException("Команда уже укомплектована");
-        } else
-            /*for (FootballPlayer player : newplayers) {
-                this.players.add(player);
-            }*/
-            Collections.addAll(this.players, newplayers);
-    }
-
-    public void removePlayer(FootballPlayer player) {
-        if (player == null) {
-            throw new IllegalArgumentException("Игрок не выбран");
-        } else
-            players.remove(player);
-    }
-
-    public int getScore() {
-        int sum = 0;
-        for (ScoringPlayer player : players) {
-            sum += player.getScore();
-        }
-        return sum;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        FootballTeam that = (FootballTeam) o;
-
-        return players != null ? players.equals(that.players) : that.players == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return players != null ? players.hashCode() : 0;
     }
 
     @Override
     public String toString() {
-        return "FootballTeam{" +
-                "players=" + players +
-                '}';
+        return "FootballTeam{}";
     }
+
 }
